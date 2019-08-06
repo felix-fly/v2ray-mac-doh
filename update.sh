3b8a257c6736d8c02d22107d5f9c643c103907c8
flyash <fenxuehaiyan@qq.com>
2019-08-06 16:24:39 +0800

    Ignore file

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# wget -O ad.hosts https://raw.githubusercontent.com/felix-fly/v2ray-dnsmasq-dnscrypt/master/ad.hosts
-# wget -O gw https://raw.githubusercontent.com/felix-fly/v2ray-dnsmasq-dnscrypt/master/gw.hosts
+wget -O ad.hosts https://raw.githubusercontent.com/felix-fly/v2ray-dnsmasq-dnscrypt/master/ad.hosts
+wget -O gw https://raw.githubusercontent.com/felix-fly/v2ray-dnsmasq-dnscrypt/master/gw.hosts
 
-# sed -e '/ipset=/d' gw > gw.hosts
-# rm gw
+sed -e '/ipset=/d' gw > gw.hosts
+rm gw
 
 # generate auto.pac file
 echo "var proxyMap = {" > auto.pac
